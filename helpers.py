d38b05fc6ba3949d69cc7f9d3d7588ef0cd31f1b
david <jarzombekdavid@gmail.com>
2018-10-24 23:20:30 -0400

    add placeholder functions
    add placeholder functions

---
+++ helpers.py
@@ -0,0 +1,46 @@
+# helping functions
+import sqlalchemy
+
+
+def sql_retrieve_user_info(email, password): 
+	pass
+
+
+def sql_retrieve_potlucks(user_id):
+	pass
+
+
+def sql_retrieve_potluck_data(potluck_id):
+	pass
+
+
+def sql_retrieve_dishes(potluck_id=null, user_id=null)
+	pass
+
+
+def sql_add_potluck(name, date):
+	pass
+
+
+def sql_invite_user(potluck_id, user_id):
+	pass
+
+
+def sql_join_potluck(potluck_id, user_id):
+	pass
+
+
+def sql_accept_dish(potluck_id, dish_name, user_id):
+	pass
+
+
+def sql_add_dish(potluck_id, dish_name, user_id=null):
+	pass
+
+
+def sql_remove_diish(potluck_id, dish_name):
+	pass
+
+
+def sql_delete_potluck(potluck_id):
+	pass
